7691960d7010b7699f7bf3decfbc480401d3c115
codeonoto <onoto0409@gmail.com>
2024-05-28 13:20:06 +0530

    added info

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,9 +1,9 @@
-"use client";
+'use client';
 
-import { PropsWithChildren, useState } from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { trpc } from "@/app/_trpc/client";
-import { httpBatchLink } from "@trpc/client";
+import { PropsWithChildren, useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { trpc } from '@/app/_trpc/client';
+import { httpBatchLink } from '@trpc/client';
 
 const Providers = ({ children }: PropsWithChildren) => {
   const [queryClient] = useState(() => new QueryClient());
@@ -11,13 +11,15 @@ const Providers = ({ children }: PropsWithChildren) => {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: "http://localhost:3000/api/trpc",
+          url: 'http://localhost:3000/api/trpc',
         }),
       ],
     })
   );
   return (
-    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+    <trpc.Provider
+      client={trpcClient}
+      queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </trpc.Provider>
   );
