a3a832605fed5747e0e6fbe736b1ac19472168df
codeonoto <onoto0409@gmail.com>
2024-05-29 13:08:12 +0530

    done with pdfrenderer

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -1,8 +1,8 @@
-import { db } from "@/app/db";
-import ChatWrapper from "@/components/ChatWrapper";
-import PdfRenderer from "@/components/PdfRenderer";
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { notFound, redirect } from "next/navigation";
+import { db } from '@/app/db';
+import ChatWrapper from '@/components/ChatWrapper';
+import PdfRenderer from '@/components/PdfRenderer';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { notFound, redirect } from 'next/navigation';
 
 interface PageProps {
   params: {
@@ -30,18 +30,18 @@ const Page = async ({ params }: PageProps) => {
   if (!file) notFound();
 
   return (
-    <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
-      <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
+    <div className='flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]'>
+      <div className='mx-auto w-full max-w-8xl grow lg:flex xl:px-2'>
         {/* Left Side */}
-        <div className="flex-1 xl:flex">
-          <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
-            <PdfRenderer />
+        <div className='flex-1 xl:flex'>
+          <div className='px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6'>
+            <PdfRenderer url={file.url} />
           </div>
         </div>
 
         {/* Right Side */}
-        <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
-            <ChatWrapper />
+        <div className='shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0'>
+          <ChatWrapper />
         </div>
       </div>
     </div>
