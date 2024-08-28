224c19c815bdf35c8a8f2c12031441e3d058a25b
Deepanshu Sahu <116287792+codeonoto@users.noreply.github.com>
2024-08-28 15:34:18 +0530

    :(

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,7 +1,11 @@
 import { db } from '@/app/db';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { createUploadthing, type FileRouter } from 'uploadthing/next';
-import { UploadThingError } from 'uploadthing/server';
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
+
+import { pc } from '@/lib/pinecone';
 
 const f = createUploadthing();
 
@@ -27,8 +31,51 @@ export const ourFileRouter = {
           uploadStatus: 'PROCESSING',
         },
       });
+      console.log(file.url);
+
+      try {
+        const response = await fetch(file.url);
+        const blob = await response.blob();
+
+        const loader = new PDFLoader(blob);
+
+        const pageLevelDocs = await loader.load();
+
+        const pagesAmt = pageLevelDocs.length;
+
+        // vectorize and index entire document
+        console.log('creating pinecone');
+        const pineconeIndex = pc.Index('myanswer');
+
+        const embeddings = new OpenAIEmbeddings({
+          openAIApiKey: process.env.OPENAI_API_KEY,
+        });
+
+        await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+          pineconeIndex,
+          namespace: createdFile.id,
+        });
+
+        await db.file.update({
+          data: {
+            uploadStatus: 'SUCCESS',
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      } catch (err) {
+        console.error('Error creating vector space:', err);
+        await db.file.update({
+          data: {
+            uploadStatus: 'FAILED',
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      }
     }),
 } satisfies FileRouter;
-
 
 export type OurFileRouter = typeof ourFileRouter;
---
+++ src/lib/pinecone.ts
@@ -0,0 +1,3 @@
+import { Pinecone } from '@pinecone-database/pinecone';
+
+export const pc = new Pinecone({ apiKey: process.env.PINECONE_API_KEY! });
